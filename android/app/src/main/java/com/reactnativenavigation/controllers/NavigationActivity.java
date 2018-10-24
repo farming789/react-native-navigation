99d4568b25bba031bb17d71c3e045c69efca4489
farming789 <yhm1025326706@126.com>
2018-10-24 15:26:04 +0800

    Update NavigationActivity.java

--- android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Window;
